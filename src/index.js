e685199c912d272d000fe45f6ad4c90a4297781e
HELK <hamza.elkhatib@protonmail.com>
2022-09-19 22:04:32 +0100

    Init

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//========================================================================================================
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    //====================================================================================================
+
+    <React.StrictMode>
+
+        <h1>This is inside index.js</h1>
+
+        <App/>
+
+    </React.StrictMode>
+
+    //====================================================================================================
 );
 
 // If you want to start measuring performance in your app, pass a function
