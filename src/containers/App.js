15e2d508e168dd514246f332e80b972d55a327c8
HELK <hamza.elkhatib@protonmail.com>
2022-09-21 10:14:43 +0100

    Migrating App.js from class to function using hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
@@ -6,79 +6,70 @@ import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 
-class App extends Component {
-
-//========================================================================================================
-    constructor() {
-
-        super();
-
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
 
 //========================================================================================================
 
-    onSearchChange = (event) => {
-
-        this.setState({searchfield: event.target.value});
-
-    }
-
-//========================================================================================================
-    render() {
-
-        const filteredRobots = this.state.robots.filter(robot => {
-
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-
-        })
-
-        if (!this.state.robots.length) {
-
-            return <h1>Loading...</h1>
-
-        } else {
-
-            return (
-
-                <div className={"tc"}>
-
-                    <h1>RoboFriends</h1>
-
-                    <SearchBox searchChange={this.onSearchChange}/>
-
-                    <Scroll>
-
-                        <ErrorBoundry>
-
-                            <CardList robots={filteredRobots}/>
-
-                        </ErrorBoundry>
-
-                    </Scroll>
-
-                </div>
-            );
-        }
-    }
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
 
 //========================================================================================================
 
-    componentDidMount() {
-
-//We fetch the data from the API, and then we map the response to json format, then we map users (which is an array of objects that matches the robots array properties) to the robots array in the state.
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => {
                 return response.json();
             })
             .then(users => {
-                this.setState({robots: users})
+                setRobots(users)
             });
+    }, []);
+
+//========================================================================================================
+
+    const onSearchChange = (event) => {
+
+        setSearchfield(event.target.value);
 
     }
+
+//========================================================================================================
+
+    const filteredRobots = robots.filter(robot => {
+
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+
+    })
+
+    if (!robots.length) {
+
+        return <h1>Loading...</h1>
+
+    } else {
+
+        return (
+
+            <div className={"tc"}>
+
+                <h1>RoboFriends</h1>
+
+                <SearchBox searchChange={onSearchChange}/>
+
+                <Scroll>
+
+                    <ErrorBoundry>
+
+                        <CardList robots={filteredRobots}/>
+
+                    </ErrorBoundry>
+
+                </Scroll>
+
+            </div>
+        );
+    }
+
+//========================================================================================================
 
 }
 
